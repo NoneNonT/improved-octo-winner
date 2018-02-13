4a27875da0171728dc0294be992021b75265f533
Gerald Koocher <geraldkoocher@macbook-air-gerald.local>
2018-02-13 12:04:01 +0300

    Initial Commit

---
+++ DigitalSkills2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DigitalSkills2
+//
+//  Created by Gerald Koocher on 13.02.2018.
+//  Copyright © 2018 Gerald Koocher. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DigitalSkills2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DigitalSkills2
+//
+//  Created by Gerald Koocher on 13.02.2018.
+//  Copyright © 2018 Gerald Koocher. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
